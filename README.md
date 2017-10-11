3d7794b0ec985dc716ccaa2e64de86257fa87abe
Arda <ardadem.dev@gmail.com>
2017-10-11 20:41:01 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Xiaomi Flashable Firmware Creator
+
+You can easily create flashable firmware zip from Stock MIUI.
+
+```
+./create_flashable_firmware ROM_FILE
+```
---
+++ create_flashable_firmware.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Usage: create_flashable_firmware.sh ROM_FILE"
+    exit
+fi
+
+if [ ! -f $1 ]; then
+    echo "** File not available."
+    exit
+fi
+
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
+
+function creatupscrpt() {
+    touch $2
+    echo "$(cat $1 | awk '/getprop/ && /ro.product.device/')" >> $2
+    echo "$(cat $1 | awk '/ui_print/ && /Target:/')" >> $2
+    echo "show_progress(0.200000, 10);" >> $2
+    echo -e "\n# Created by Xiaomi Flashable Firmware Creator - $DATE\n" >> $2
+    echo 'ui_print("Patching firmware images...");' >> $2
+    echo "$(cat $1 | awk '/package_extract_file/ && /firmware-update\//')" >> $2
+    echo "show_progress(0.100000, 2);" >> $2
+    echo "set_progress(1.000000);" >> $2
+}
+
+mkdir temp/
+mkdir temp/unzipped
+unzip $1 -d temp/unzipped/
+
+if [ ! -f temp/unzipped/META-INF/com/google/android/update-binary ] || [ ! -f temp/unzipped/META-INF/com/google/android/updater-script ] || [ ! -d temp/unzipped/firmware-update ]; then
+    echo "** This zip doesn't contain firmware directory."
+    rm -rf temp/
+    exit
+fi
+
+mv temp/unzipped/firmware-update temp/
+
+mkdir -p temp/META-INF/com/google/android
+mv temp/unzipped/META-INF/com/google/android/update-binary temp/META-INF/com/google/android/
+creatupscrpt temp/unzipped/META-INF/com/google/android/updater-script temp/META-INF/com/google/android/updater-script
+
+cd temp/
+zip -r ../fw_$1 META-INF/ firmware-update/
+cd ../
+
+rm -rf temp/
