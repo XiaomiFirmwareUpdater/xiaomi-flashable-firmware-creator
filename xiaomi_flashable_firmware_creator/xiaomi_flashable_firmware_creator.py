9ac80858535085b019b5e162ba2a3e06abdafa17
yshalsager <ysh-alsager@hotmail.com>
2020-10-15 14:29:43 +0200

    Add missing output parameter to arguments parser
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- xiaomi_flashable_firmware_creator/xiaomi_flashable_firmware_creator.py
+++ xiaomi_flashable_firmware_creator/xiaomi_flashable_firmware_creator.py
@@ -7,7 +7,7 @@ from xiaomi_flashable_firmware_creator.extractors.local_extractor import LocalEx
 from xiaomi_flashable_firmware_creator.extractors.remote_extractor import RemoteExtractor
 
 
-def arg_parse() -> (str, str):
+def arg_parse() -> (str, str, str):
     """
     Parses command-line arguments
     :return: process, file
@@ -19,18 +19,25 @@ def arg_parse() -> (str, str):
     group.add_argument("-N", "--nonarb", help="Create non-ARB Firmware zip")
     group.add_argument("-L", "--firmwareless", help="Create Firmware-less zip")
     group.add_argument("-V", "--vendor", help="Create Firmware+Vendor zip")
+    parser.add_argument('-o', "--output", help="Output directory")
     args = parser.parse_args()
+    if args.output:
+        output = args.output
+        del args.output
+    else:
+        output = None
     choose = {k: v for k, v in vars(args).items() if v is not None}
     process, zip_ = list(choose.items())[0]
-    return process, zip_
+    return process, zip_, output
 
 
 def main():
     """
     Xiaomi Flashable Firmware Creator
     """
-    process, zip_ = arg_parse()
-    extractor = RemoteExtractor(process, zip_) if "http" in zip_ else LocalExtractor(process, zip_)
+    process, zip_, output = arg_parse()
+    extractor = RemoteExtractor(process, zip_, output) \
+        if "http" in zip_ else LocalExtractor(process, zip_, output)
     print(f"Unzipping MIUI... ({extractor.zip_type.name}) device")
     extractor.extract()
     print("Generating updater-script..")
